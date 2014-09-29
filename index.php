49b87d2f687a4a7bc38262c944988b79c43b7df0
EC2 Default User <ec2-user@ip-10-32-18-78.ec2.internal>
2014-09-29 18:20:32 +0000

    updates

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
+<html xml:lang="en" lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+<title>Brooke Gravitt</title>
+	
+<meta name="description" content="Natural Philosopher, antagonizer of Leibniz & member of The Royal Society since 1673." />
+<link rel="shortcut icon" href="http://www.gravatar.com/avatar/adb162cd51a6d568dd64003ed1af3220.png">	
+<script type="text/javascript">
+			function show(id) {
+				document.getElementById(id).style.display = 'block';
+			}
+			function hide(id) {
+				document.getElementById(id).style.display = 'none';
+			}
+		</script>
+
+<style> a:link, a:visited, a:active, a:hover
+{
+color: white;
+}
+ul a 
+{
+font-size: 21px;
+color: blue;
+}
+</style>			
+<link rel="stylesheet" type="text/css" href="css/site.css">
+<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" type="text/css" media="screen and (max-width: 480px)" href="css/small.css" />
+</head>
+<body>
+<br />
+<br />
+<br />
+<h1 style="font-size:75px"> <center> Brooke Gravitt </center> </h1>
+<center><p> Solutions Architect, Developer, Husband, Father.</p>
+     <p> Brooke likes to make stuff, mash it together, and then automate it.</p> 
+</center> 
+
+		<div class="content">
+			<div id="social-com">
+			<center>
+			<a href="http://www.facebook.com/brookegravitt"><img src="images/facebook_32x32.png" title="Facebook" alt="Facebook" /></a>
+				<a href="http://twitter.com/brookegravitt"><img src="images/twitter_32x32.png" title="Twitter" alt="Twitter" /></a>
+				<a href="mailto:brooke@gravitt.org"><img src="images/email_32x32.png" title="Email" alt="Email" /></a>
+			<a href="gtalk:chat?jid=gravitt@gmail.com"><img src="images/gtalk_32x32.png" title="Google Chat" alt="Google Chat" /></a>
+				<a href="http://in.linkedin.com/in/brookegravitt"><img src="images/linkedin_32x32.png" title="LinkedIn" alt="LinkedIn" /></a>
+				<a href="https://github.com/brookegravitt"><img src="images/github_32x32.png" title="GitHub" alt="GitHub" /></a>
+				<a href="skype:brookegravitt?call"><img src="images/skype_32x32.png" title="Skype Call" alt="Skype Call" /></a>
+			</center>
+			</div>
+
+
+			<p> <span style="position:absolute;right;0;bottom:0"> <end>&copy 2012 Brooke Gravitt</span> </p>
+			</body>
+			
+            <!-- inspired by Samrat Mazumdar's website http://www.iamsamrat.com  -->
+</html>
